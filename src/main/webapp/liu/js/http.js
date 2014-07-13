0e0d4147f6a0e34ba2da2e11e99b9b0d40ee6cf3
liuzheng <liuzheng712@gmail.com>
2014-07-13 10:56:45 +0800

    http.js是http请求的模板

---
+++ src/main/webapp/liu/js/http.js
@@ -0,0 +1,22 @@
+/**
+ * Created by liuzheng on 2014/7/13.
+ */
+$http({
+    url: "/user/setting/query",
+    method: 'POST',
+    headers: {
+        "Authorization": Data.token
+    },
+    data: {"uid": Data.uid}
+}).success(function (data) {
+    $scope.state = data["state"];//1 true or 0 false
+    Data.token = data["token"];
+    $scope.message = data["message"];
+    if ($scope.state) {
+//仅需要对message中的数据做处理
+    } else {
+
+    }
+}).error(function (data) {
+
+});
