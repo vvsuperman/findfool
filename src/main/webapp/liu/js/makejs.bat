04b98a560ddda091e9a1748579bf396c27335422
liuzheng <liuzheng712@gmail.com>
2014-07-29 20:08:15 +0800

    update makejs to make js file be one

--- src/main/webapp/liu/js/makejs.bat
+++ src/main/webapp/liu/js/makejs.bat
@@ -1,3 +1,2 @@
-copy marked.js+showdown.js+markdown
-_preview.js+md5.js+xls.js+des.js zeit.js /b
+copy marked.js+showdown.js+markdown_preview.js+md5.js+xls.js+des.js zeit.js /b
 uglifyjs zeit.js -m -o zeit.min.js
