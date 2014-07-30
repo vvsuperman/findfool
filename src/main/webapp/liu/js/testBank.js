884878a1d4645f27e01657b9b4d7e02a11f01910
U-AP\peilongz <peilongz@PEILONGZ.ap.qualcomm.com>
2014-07-30 17:08:06 +0800

    把cleditor换成了ckeditor

--- src/main/webapp/liu/js/testBank.js
+++ src/main/webapp/liu/js/testBank.js
@@ -50,14 +50,6 @@ function TestBank($scope, $http,Data,$sce) {
     $scope.active = 1;
     $scope.show = 1;
     $scope.privi = Data.privi();
-    //add by zpl
-	var flag = Data.flag();
-	if(flag == 0){
-		Data.setFlag(1);
-		window.location.reload();
-	}else{
-		Data.setFlag(0);		
-	}
     $scope.reciveData = new Object();
     $scope.reciveData.selectedSets = null;
     $scope.reciveData.totalPage = 1;
