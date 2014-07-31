5d35152f658ca37b2d25e670192e9047cc33d55a
liuzheng <liuzheng712@gmail.com>
2014-07-31 20:44:30 +0800

    修改了makejs.bat

--- src/main/webapp/liu/js/makejs.bat
+++ src/main/webapp/liu/js/makejs.bat
@@ -1,2 +1,2 @@
-copy jquery.cleditor.js+marked.js+showdown.js+markdown_preview.js+md5.js+xls.js+des.js+scrollsyp.js zeit.js /b
+copy marked.js+showdown.js+markdown_preview.js+md5.js+xls.js+des.js+scrollsyp.js zeit.js /b
 uglifyjs zeit.js -m -o zeit.min.js
