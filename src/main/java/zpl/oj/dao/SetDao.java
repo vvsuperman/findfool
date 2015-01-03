bfbb4cf7fc4dbbfc15afd2ddc40703327a250671
vvsuperman <fw@192.168.1.103>
2015-01-03 19:52:03 +0800

    加入图片

--- src/main/java/zpl/oj/dao/SetDao.java
+++ src/main/java/zpl/oj/dao/SetDao.java
@@ -11,23 +11,23 @@ import zpl.oj.model.common.ProblemSet;
 public interface SetDao {
 
 	@Select("select PROBLEM_SET_ID as problemSetId, NAME, DATE,DOMAN_ID AS domainId OWNER,COMMENT"
-			+ "  FROM SETS  WHERE problem_set_id = #{0}")
+			+ "  FROM SETS  WHERE problem_set_id = #{0} and state=1")
 	ProblemSet getSet(int id);
 	
 	@Select("select PROBLEM_SET_ID as problemSetId, NAME, DATE,DOMAN_ID AS domainId, OWNER,COMMENT"
-			+ "  FROM SETS  WHERE name = #{0}")
+			+ "  FROM SETS  WHERE name = #{0} and state=1")
 	ProblemSet getSetByName(String name);
 
-	@Insert("INSERT INTO SETS(NAME,  DATE, OWNER,DOMAN_ID AS domainId,COMMENT)"
-			+ "VALUES(#{name}, #{date},#{owner},#{domanId},#{comment})")
+	@Insert("INSERT INTO SETS(NAME,  DATE, OWNER,DOMAN_ID AS domainId,COMMENT,state)"
+			+ "VALUES(#{name}, #{date},#{owner},#{domanId},#{comment},1)")
 	void insertSet(ProblemSet set);
 	
 	@Select("select PROBLEM_SET_ID as problemSetId, NAME, DATE, OWNER,DOMAN_ID AS domainId,COMMENT"
-			+ "  FROM SETS")
+			+ "  FROM SETS where state=1")
 	List<ProblemSet> getSets();
 	
 	@Select("select PROBLEM_SET_ID as problemSetId, NAME, DATE, OWNER,DOMAIN_ID AS domainId,COMMENT"
-			+ "  FROM SETS WHERE DOMAIN_ID=#{0}")
+			+ "  FROM SETS WHERE DOMAIN_ID=#{0} and state=1")
 	List<ProblemSet> getSetByDomainId(int domainId);
 
 }
