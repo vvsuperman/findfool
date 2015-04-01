649e770572871d45dae8865baf95ec1a24fed71e
GilbertGan <648840731@qq.com>
2015-04-01 17:42:39 +0800

    loadingTip

--- src/main/webapp/resource/js/directive/message.js
+++ src/main/webapp/resource/js/directive/message.js
@@ -7,10 +7,26 @@ function flashTip(string){
 	hint.setAttribute('class','tipBox');
 	hint.appendChild(content);
 	document.body.appendChild(hint);		
-	setTimeout("remove()",2000);
+	setTimeout("removeTip()",2000);
 }
 
-function remove(){
+function removeTip(){
 	document.body.removeChild(document.body.lastChild);
+}
+
+function loadingTip(){
+	var container=document.createElement('div');
+	container.setAttribute('id','container');
+	container.setAttribute('class','loadingTip-container');
+	var content=document.createElement('div');
+	content.setAttribute('class','loadingTip-content');
+	var img=document.createElement('img');
+	img.setAttribute('src','./resource/static/loadingTip.gif');
+	var p=document.createElement('p');
+	p.innerHTML="正在处理...";
+	content.appendChild(img);
+	content.appendChild(p);
+	container.appendChild(content);
+	document.body.appendChild(container);
 }
 
