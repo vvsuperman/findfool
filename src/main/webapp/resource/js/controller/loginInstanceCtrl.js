b443b5b2aa393d16a7f90d0226b9cdcc22ed2496
yuzhan.zhang <the2th@qq.com>
2015-04-03 09:53:25 +0800

    1、修改了页面顶部响应式按钮的问题。 2、将首页的login登陆框改为modal控件方式。

---
+++ src/main/webapp/resource/js/controller/loginInstanceCtrl.js
@@ -0,0 +1,69 @@
+OJApp.controller('loginInstanceCtrl',function ($scope,$http,$modalInstance,Data) {
+	
+	//手机的错误信息
+	$scope.errorMsg={};
+	$scope.verifyQtn ={};
+	
+	$scope.mdLogin = function(){
+		 var url = 'https://api.mingdao.com/oauth2/authorize?app_key=EB56F580B240&redirect_uri='+MD_REDIRECT
+		 window.location.href= url;
+	 } 
+	$scope.confirm = function (data) {
+        if ($scope.Lemail && $scope.Lpwd) {
+        	if(data==1){
+        		var pwd = md5($scope.Lpwd);
+        	}else{
+        		var pwd = $scope.Lpwd;
+        	}
+        	
+        	
+            $http({
+                url: WEBROOT+"/user/confirm",
+                method: 'POST',
+                headers: {
+                    "Authorization": Data.token()
+                },
+                data: {"email": $scope.Lemail, "pwd": pwd, "name": $scope.Lname}
+            }).success(function (data) {
+                $scope.state = data["state"];//1 true or 0 false
+                Data.clear();//清空缓存
+                var name = $scope.Lemail;
+                $scope.message = data["message"];
+                if (data["message"].handler_url != null && data["message"].handler_url !== "") {
+                    name = data["message"].handler_url;
+                }
+                Data.setName(name);
+                Data.setEmail($scope.Lemail);
+                
+
+                if ($scope.state) {
+                    Data.setToken(data["token"]);
+                    Data.setUid($scope.message.uid);
+                    Data.setPrivi($scope.message.privilege);
+                    Data.setTel($scope.message.tel);
+                    Data.setCompany($scope.message.company);
+                    Data.setInvitedleft($scope.message.invited_left);
+                    Data.setLastActive((new Date()).getTime());
+                    $scope.invitedleft = $scope.message.invited_left;
+                   
+                    window.location.href = '#/loginok';
+//                    $scope.name = $scope.message.handler_url;
+                } else {
+                    
+//                    window.location.href = '#/loginok';
+                	$scope.errmsg = data.message.msg;
+                }
+            }).error(function () {
+//                    alert("网络错误");
+//                    window.location.reload(true);
+                }
+            )
+        }
+    };
+	$scope.cancel = function(){
+		$modalInstance.dismiss('cancel');
+	}
+	
+	
+	
+});
