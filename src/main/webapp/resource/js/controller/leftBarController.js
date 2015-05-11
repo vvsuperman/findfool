abdca867a9311a48155aae64c4dc8266bd856a2b
vvsuperman <fw@192.168.1.103>
2015-05-11 09:39:10 +0800

    小bug修改，上线前修改

--- src/main/webapp/resource/js/controller/leftBarController.js
+++ src/main/webapp/resource/js/controller/leftBarController.js
@@ -1,4 +1,10 @@
 OJApp.controller('leftBarController',function($scope,Data){
-	$scope.tname = Data.tname();
+	$scope.$watch("Data.tname()",function(){
+		console.log("name change");
+		$scope.tname = Data.tname();
+		
+	});
 	$scope.tid = Data.tid();
+	
+	
 });
