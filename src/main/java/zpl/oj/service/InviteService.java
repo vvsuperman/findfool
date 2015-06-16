580eb8de037fe0064da0a85d6ac500b805296239
vvsuperman <fw@192.168.1.103>
2015-06-16 14:40:08 +0800

    漏掉的

--- src/main/java/zpl/oj/service/InviteService.java
+++ src/main/java/zpl/oj/service/InviteService.java
@@ -9,7 +9,6 @@ import java.io.UnsupportedEncodingException;
 import org.apache.http.client.ClientProtocolException;
 
 import zpl.oj.model.common.Quiz;
-import zpl.oj.model.common.Testuser;
 import zpl.oj.model.request.InviteUser;
 import zpl.oj.model.request.User;
 import zpl.oj.model.requestjson.RequestTestInviteUser;
