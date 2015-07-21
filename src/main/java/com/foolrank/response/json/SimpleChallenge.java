bfc591bea10e5907226a84147ec3efb567fe2dc5
Moon <moon.ghost.jian@gmail.com>
2015-07-21 18:12:06 +0800

    增加挑战赛返回格式

---
+++ src/main/java/com/foolrank/response/json/SimpleChallenge.java
@@ -0,0 +1,67 @@
+package com.foolrank.response.json;
+
+public class SimpleChallenge {
+	private int id;
+	private String name;
+	private String logo;
+	private String description;
+	private String key;
+	private String startTime;
+	private String endTime;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(String startTime) {
+		this.startTime = startTime;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+
+	public String getLogo() {
+		return logo;
+	}
+
+	public void setLogo(String logo) {
+		this.logo = logo;
+	}
+}
