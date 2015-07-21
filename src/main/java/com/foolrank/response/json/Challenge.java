cd2b513055df99639cc1aa709aeeb597a7192051
Moon <moon.ghost.jian@gmail.com>
2015-07-21 15:44:58 +0800

    新增挑战赛、公司的返回格式

---
+++ src/main/java/com/foolrank/response/json/Challenge.java
@@ -0,0 +1,17 @@
+package com.foolrank.response.json;
+
+/**
+ * 挑战赛对象
+ * 
+ * @author Moon
+ */
+public class Challenge {
+	private String name;
+	private String type;
+	private String description;
+	private String prize;
+	private String start;
+	private String end;
+	private int status;
+	private String key;
+}
---
+++ src/main/java/com/foolrank/response/json/Company.java
@@ -0,0 +1,9 @@
+package com.foolrank.response.json;
+
+public class Company {
+	private int id;
+	private String name;
+	private String address;
+	private String tel;
+	private String website;
+}
