7c9eec7691fffe00a5e666ff7d5e492f7b00c968
Moon <moon.ghost.jian@gmail.com>
2015-07-23 23:38:48 +0800

    增加provider

---
+++ src/main/java/com/foolrank/provider/QuizProvider.java
@@ -0,0 +1,29 @@
+package com.foolrank.provider;
+
+import java.util.List;
+import java.util.Map;
+
+import zpl.oj.model.request.User;
+
+public class QuizProvider {
+
+	public String getChallengeListByUsers(Map<String, Object> map) {
+		List<User> users = (List<User>) map.get("users");
+		int status = (int) map.get("status");
+		int offset = (int) map.get("offset");
+		int count = (int) map.get("count");
+		StringBuilder sb = new StringBuilder();
+		sb.append("SELECT * FROM quiz WHERE type=1 AND owner in (");
+		for (int i = 0; i < users.size(); i++) {
+			sb.append(users.get(i).getUid());
+			if (i < users.size() - 1) {
+				sb.append(",");
+			}
+		}
+		sb.append(") AND status=").append(status);
+		sb.append(" ORDER BY start_time ASC LIMIT ").append(offset);
+		sb.append(",").append(count);
+
+		return sb.toString();
+	}
+}
