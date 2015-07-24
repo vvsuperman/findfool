4109b2166ff7f117a3d65669b54f3b918ebe5ae4
hanyaquan <705216797@qq.com>
2015-07-24 14:39:32 +0800

    实现cs端登录注册功能；

--- src/main/java/zpl/oj/dao/TestuserDao.java
+++ src/main/java/zpl/oj/dao/TestuserDao.java
@@ -8,26 +8,42 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import zpl.oj.model.common.Testuser;
-
-
+import zpl.oj.model.request.User;
 
 public interface TestuserDao {
-	 
-  @Insert("INSERT INTO testuser( username,email,school,company,blog,age,tel,registerdate,lastlogindate,state,discipline,gratime,pwd)"
+
+	@Insert("INSERT INTO testuser( username,email,school,company,blog,age,tel,registerdate,lastlogindate,state,discipline,gratime,pwd)"
 			+ " VALUES( #{username}, #{email}, #{school}, #{company},#{blog},#{age},#{tel},#{registerDate},#{lastLoginDate},#{state},#{discipline},#{gratime},#{pwd})")
-  void insertTestuser(Testuser testuser);
-  
-  @Update("update testuser set username=#{username},email=#{email} ,school=#{school},company=#{company},blog=#{blog},age=#{age},tel=#{tel},discipline=#{discipline},gratime=#{gratime},"
-  		+ "registerdate = #{registerDate},lastlogindate=#{lastLoginDate},faceid=#{faceid}  where tuid =#{tuid}")
-  void updateTestuserById(Testuser testuser);
-  
-  @Select("select * from testuser where email = #{0}")
-  Testuser findTuserByEmail(String email);
-  
-  @Select("select * from testuser where tel = #{0}")
-  Testuser findTuserByTel(String tel);
-  
-  @Select("select * from testuser where tuid = #{0}")
-  Testuser findTestuserById(int tuid);
-    
+	void insertTestuser(Testuser testuser);
+
+	@Update("update testuser set username=#{username},email=#{email} ,school=#{school},company=#{company},blog=#{blog},age=#{age},tel=#{tel},discipline=#{discipline},gratime=#{gratime},"
+			+ "registerdate = #{registerDate},lastlogindate=#{lastLoginDate},faceid=#{faceid}  where tuid =#{tuid}")
+	void updateTestuserById(Testuser testuser);
+
+	@Select("select * from testuser where email = #{0}")
+	Testuser findTuserByEmail(String email);
+
+	@Select("select * from testuser where tel = #{0}")
+	Testuser findTuserByTel(String tel);
+
+	@Select("select * from testuser where tuid = #{0}")
+	Testuser findTestuserById(int tuid);
+
+	@Update("update testuser set lastlogindate=now() where email = #{email}")
+	void updateLoginDateByEmail(String email);
+
+	@Update("update testuser set lastlogindate=now() where tuid = #{tuid}")
+	void updateLoginDateByUid(int tuid);
+
+	@Select("select pwd from testuser where email = #{0}")
+	String findTuserByPwd(String email);
+
+	@Update("update testuser set username=#{username},email=#{email} ,school=#{school},company=#{company},blog=#{blog},age=#{age},tel=#{tel},discipline=#{discipline},gratime=#{gratime},"
+			+ "registerdate = #{registerDate},lastlogindate=#{lastLoginDate},faceid=#{faceid}  where tuid =#{0}")
+	void updateTestuserById(int tuid);
+
+	
+	@Update("update testuser set username=#{username},pwd=#{pwd},tel=#{tel} where email =#{email}")
+	void updateTestuser(Testuser test1);
+
 }
--- src/main/java/zpl/oj/web/Rest/tcontroller/TuserController.java
+++ src/main/java/zpl/oj/web/Rest/tcontroller/TuserController.java
@@ -1,87 +1,162 @@
-//package zpl.oj.web.Rest.tcontroller;
-//
+package zpl.oj.web.Rest.tcontroller;
+
 //import java.text.SimpleDateFormat;
 //import java.util.Date;
 //import java.util.HashMap;
 //import java.util.Map;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import zpl.oj.dao.InviteDao;
-//import zpl.oj.dao.QuizDao;
-//import zpl.oj.dao.TestuserDao;
-//import zpl.oj.model.common.Candidate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import zpl.oj.dao.TestuserDao;
 //import zpl.oj.model.common.Invite;
 //import zpl.oj.model.common.Quiz;
-//import zpl.oj.model.common.Testuser;
+import zpl.oj.model.common.Testuser;
+import zpl.oj.model.requestjson.RequestUserLogin;
 //import zpl.oj.model.common.VerifyQuestion;
-//import zpl.oj.model.responsejson.ResponseBase;
-//import zpl.oj.service.VerifyQuestionService;
-//import zpl.oj.service.imp.CadService;
-//import zpl.oj.util.Constant.ExamConstant;
-//import zpl.oj.web.Rest.Controller.TestingController.JsonLable;
-//
-//import com.google.gson.Gson;
-//
-//
-////客户端做挑战赛的控制器
-//
-//@Controller
-//@RequestMapping("/tuser")
-//public class TuserController {
-//	@Autowired
-//    TestuserDao tuserDao;
-//	
-//	@Autowired
-//	private VerifyQuestionService verifyQuestionService;
-//	
-//	@Autowired
-//	private CadService cadService;
-//	
-//	
-//	@Autowired
-//	private QuizDao quizDao;
-//	
-//	@Autowired
-//	private InviteDao inviteDao;
-//
-//	
-//	//注册步骤1
-//	@RequestMapping(value = "/preregister", method = RequestMethod.POST)
-//	@ResponseBody
-//	public ResponseBase preRegister(@RequestBody Testuser tuser) {
-//		ResponseBase rb = new ResponseBase();
-//		
-//		if(tuser.getEmail()==null||tuser.getTel()==null||tuser.getPwd()==null){
-//			rb.setState(3);
-//			rb.setMessage("输入项不得为空，请重新输入");
-//		}
-//		
-//		
-//		if(tuserDao.findTuserByEmail(tuser.getEmail())!=null){
-//			rb.setState(1);
-//			rb.setMessage("email已注册，请直接登陆");
-//			return rb;
-//		}
-//		
-//		if(tuserDao.findTuserByTel(tuser.getTel())!=null){
-//			rb.setState(2);
-//			rb.setMessage("手机号码已被注册，");
-//			return rb;
-//		}
-//		
-//		tuserDao.insertTestuser(tuser);
-//		rb.setState(0);
-//		rb.setMessage(tuser.getEmail());
-//		return rb;
-//		
-//	}
-//	
+import zpl.oj.model.responsejson.ResponseBase;
+import zpl.oj.model.responsejson.ResponseMessage;
+import zpl.oj.service.VerifyQuestionService;
+import zpl.oj.service.imp.CadService;
+import zpl.oj.service.imp.TuserService;
+import zpl.oj.util.Constant.ExamConstant;
+import zpl.oj.web.Rest.Controller.TestingController.JsonLable;
+import zpl.oj.util.Constant.ExamConstant;
+import zpl.oj.util.sms.SMS;
+
+import com.google.gson.Gson;
+
+
+//客户端做挑战赛的控制器
+
+@Controller
+@RequestMapping("/tuser")
+public class TuserController {
+	private static final String String = null;
+
+	@Autowired
+    TestuserDao tuserDao;
+	
+	@Autowired
+	private SMS sms;
+	
+	@Autowired
+	TuserService tuserService;
+	
+	
+	
+	// 获取手机验证码
+		@RequestMapping(value = "/getvertifycode", method = RequestMethod.POST)
+		@ResponseBody
+		public ResponseBase getVertifyCode(@RequestBody Map map1) {
+			ResponseBase rb = new ResponseBase();
+			String mobile = (String) map1.get("mobile");
+			// 发送短信
+			int max = 9999;
+			int min = 1000;
+			Random random = new Random();
+			int s = random.nextInt(max) % (max - min + 1) + min;
+			List<String> ls = new ArrayList<String>();
+			ls.add(s + "");
+			sms.send(mobile, ls, ExamConstant.SMS_TEMPID_REMIND);
+			rb.setMessage(s);
+			return rb;
+		}
+
+		
+		@RequestMapping(value = "/confirm")
+		@ResponseBody
+		public ResponseBase userLogin(@RequestBody RequestUserLogin request) {
+			ResponseBase rb = new ResponseBase();
+			Testuser u = new Testuser();
+			ResponseMessage msg = new ResponseMessage();
+	
+			u = tuserService.findTuserByEmail(request.getEmail());
+			
+			if (u== null) {
+				msg.setMsg("usernotexist");
+				rb.setState(1);
+				rb.setMessage(msg);
+				return rb;
+			}
+			else if((u.getPwd()!=null)&&(!u.getPwd().equals(request.getPwd()))) {
+			
+				msg.setMsg("错误的用户名或密码");
+				msg.setHandler_url("/error");
+				rb.setState(2);
+				rb.setMessage(msg);
+				return rb;
+			}
+			else {
+				u = tuserService.userLogin(u.getTuid());
+				rb.setMessage(u);
+				rb.setState(0);
+			//	rb.setToken(securityService.computeToken(u));
+			}
+			return rb;
+		}
+		
+
+		
+	
+	
+	//注册步骤
+	@RequestMapping(value ="/register", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseBase register(@RequestBody Testuser tuser) {
+		ResponseBase rb = new ResponseBase();
+		Testuser tuser1=tuser;
+		if(tuser1.getEmail()==null||tuser1.getTel()==null||tuser1.getPwd()==null){
+			rb.setState(3);
+			rb.setMessage("输入项不得为空，请重新输入");
+			return rb;
+		}
+		//如果邮箱存在，就要返回这个对象，进行更新用户信息的操作
+		Testuser tuser2=tuserDao.findTuserByEmail(tuser.getEmail());
+		
+			//邮箱不存在，进行插入操作
+			if(tuser2==null){
+				
+				tuserDao.insertTestuser(tuser1);
+				rb.setState(0);
+				rb.setMessage(tuser1.getEmail());
+				return rb;
+			
+			}
+			
+		
+			if(((tuser2.getEmail())!=null)&&((tuser2.getPwd())!=null)){
+				rb.setState(1);
+				rb.setMessage("email已注册，请直接登陆");
+				System.out.println("email已注册，请直接登陆");
+				return rb;
+			}
+			else if(tuser2.getEmail()!=null){
+				tuserService.updateTestuser(tuser1);
+				rb.setState(0);
+				rb.setMessage(tuser2.getEmail());
+				return rb;
+			}
+			return rb;
+	
+		
+	}
+
+
+	
+
+	
 //	//注册步骤,注册完毕 testid email hrid
 //	@RequestMapping(value = "/register", method = RequestMethod.POST)
 //	@ResponseBody
@@ -97,11 +172,11 @@
 //			rb.setMessage("输入不得为空");
 //			return rb;
 //		}
-//		//生成invite
-//		
-//		Invite invite = new Invite();
-//		//
-//		
+////		//生成invite
+////		
+////		Invite invite = new Invite();
+////		//
+////		
 //		invite.setTestid(testid);
 //		invite.setHrid((Integer)params.get("hrid"));
 //		invite.setUid(tuid);
@@ -285,7 +360,7 @@
 //			rb.setState(0);
 //			return rb;
 //		}
-//
+
 //		// 验证url是否合法
 //		@RequestMapping(value = "/setting/checkurl")
 //		@ResponseBody
@@ -307,7 +382,7 @@
 //			rb.setMessage(cad.getEmail());
 //			return rb;
 //		}
-//
+
 //		// 重置密码
 //		@RequestMapping(value = "/setting/resetpwd")
 //		@ResponseBody
@@ -332,6 +407,104 @@
 //			rb.setState(0);
 //			return rb;
 //		}
+	
+//	@RequestMapping(value = "/oauthorlogin")
+//	@ResponseBody
+//	public ResponseBase oauthorLogin(@RequestBody Map map) {
+//		ResponseBase rb = new ResponseBase();
+//		String source = (String) map.get("source");
+//
+//		if (ExamConstant.SOURCE_MD.equals(source)) {
+//			String code = (String) map.get("code");
+//			String token = Config.getAccessTokenByCode(code);
+//			// 访问api出错，直接返回
+//			if (token == null) {
+//				rb.setState(3);
+//				return rb;
+//			}
+//			ObjectMapper mapper = new ObjectMapper();
+//			Map<String, Object> tokenMap = new HashMap<String, Object>();
+//			try {
+//				tokenMap = mapper.readValue(token, Map.class);
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			token = (String) tokenMap.get("access_token");
+//
+//			String user = Config.getUserInfo(token);
+//			if (user == null) {
+//				rb.setState(3);
+//				return rb;
+//			}
+//			Map<String, Object> userMap = new HashMap<String, Object>();
+//			try {
+//				userMap = mapper.readValue(user, Map.class);
+//			} catch (Exception e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//
+//			userMap = (Map) userMap.get("user");
+//			User u = userService.getUserByEmail((String) userMap.get("email"));
+//			// 用户已存在，登陆
+//			Map rtMap = new HashMap<String, Object>();
+//			if (u != null) {
+//				rb.setState(1);
+//				rtMap.put("user", u);
+//				rtMap.put("token", token);
+//				rb.setMessage(rtMap);
+//			} else {
+//				u = new User();
+//				u.setEmail((String) userMap.get("email"));
+//				u.setCompany((String) userMap.get("company"));
+//				u.setMdUid((String) userMap.get("id"));
+//				rtMap.put("user", u);
+//				rtMap.put("token", token);
+//				rb.setState(2);
+//				rb.setMessage(rtMap);
+//			}
+//		}
+//		return rb;
+//	}
+//
+//	
+	
+//	
+//	@RequestMapping(value = "/add/admin")
+//	@ResponseBody
+//	public ResponseBase addAdminUser(@RequestBody RequestUserLogin request) {
+//		ResponseBase rb = new ResponseBase();
+//		User u = new User();
+//		u.setFname(request.getName());
+//		u.setEmail(request.getEmail());
+//		u.setPwd(request.getPwd());
+//		// 默认等级
+//		u.setPrivilege(3);
+//		ResponseMessage msg = new ResponseMessage();
+//		boolean res = userService.addUser(u);
+//
+//		if (res == false) {
+//			msg.setMsg("注册失败，邮箱已被注册");
+//			msg.setHandler_url("/error");
+//			rb.setState(0);
+//			rb.setMessage(msg);
+//		} else {
+//			u = userService.getUserByEmail(u.getEmail());
+//			userService.userLogin(u.getUid());
+//			msg.setMsg(new String() + u.getUid());
+//			rb.setState(1);
+//			rb.setToken(securityService.computeToken(u));
+//			rb.setMessage(u);
+//		}
+//
+//		return rb;
+//	}
+////登录模块实现
 //
 //
-//}
+
+
+
+
+}
