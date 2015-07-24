17efb6beb0345e4ef12a6123f250851065e73b1a
Moon <moon.ghost.jian@gmail.com>
2015-07-24 15:09:25 +0800

    增加修改公司信息接口

--- src/main/java/zpl/oj/dao/CompanyDao.java
+++ src/main/java/zpl/oj/dao/CompanyDao.java
@@ -3,16 +3,19 @@ package zpl.oj.dao;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.foolrank.model.CompanyModel;
 
 public interface CompanyDao {
 
-	@Insert("INSERT INTO company(name,cover,logo,address,tel,website,description)"
-			+ " VALUES (#{name},#{cover},#{logo},#{address},#{tel},#{website},#{description})")
+	@Insert("INSERT INTO company(name,cover,logo,address,tel,website,description) VALUES (#{name},#{cover},#{logo},#{address},#{tel},#{website},#{description})")
 	@Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
 	int add(CompanyModel company);
-	
+
 	@Select("SELECT * FROM company WHERE id=#{0} LIMIT 1")
 	CompanyModel getById(int id);
+
+	@Update("UPDATE company SET name=#{name},cover=#{cover},logo=#{logo},address=#{address},tel=#{tel},website=#{website},description=#{description} WHERE id=#{id}")
+	void modify(CompanyModel company);
 }
