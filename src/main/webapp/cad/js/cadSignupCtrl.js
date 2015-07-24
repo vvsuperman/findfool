8916de9a761befff46e6c42feb947f927ec61166
hanyaquan <705216797@qq.com>
2015-07-24 17:23:32 +0800

    提交前端的js

---
+++ src/main/webapp/cad/js/cadSignupCtrl.js
@@ -0,0 +1,74 @@
+OJApp.controller('cadSignupCtrl',['$scope','$http','Data',function ($scope,$http,Data) {
+	
+	$scope.Remail;
+//	$scope.company;
+	$scope.name;
+	//手机的错误信息
+	$scope.errorMsg={};
+	$scope.verifyQtn ={};
+	console.log("dadada");
+	$scope.$on("mobileError",function(event,data){
+		$scope.$apply(function(){
+			$scope.errorMsg.mobile = data;
+		});
+	});
+	
+	$scope.$on("vertifycode",function(event,data){
+		$scope.$apply(function(){
+			$scope.verifyQtn.answer = data;
+		});
+	})
+
+	
+	//绑定变量到服务
+	$scope.addhr = function () {
+        if ($scope.Remail && $scope.Rpwd && $scope.verifyAns) {
+            if ($scope.Rpwd !=null) {
+            
+                if ($scope.verifyAns == $scope.verifyQtn.answer) {
+                	//邮箱才可注册
+                	var re = /^([a-zA-Z0-9]+[_|\-|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\-|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;	
+ 					
+ 				    if(!re.test($scope.Remail)){
+ 						$scope.errorEmail = "请输入正确的email格式1";
+ 						return false;
+ 					} 	
+				
+                    $http({
+                        url: WEBROOT+"/tuser/register",
+                        method: 'POST',
+                        headers: {
+                            "Authorization": Data.token()
+                        },
+                        data: {"email": $scope.Remail, "pwd": md5(($scope.Rpwd).toLowerCase()), "username": $scope.name,"tel":$scope.mobile}
+                    }).success(function (data) {
+                        $scope.state = data["state"];//1 true or 0 false
+                        $scope.message = data["message"];
+                        if ($scope.state==0) {
+ 
+                            Data.setToken(data["token"]);
+                            //end bu zpl
+                            Data.setName($scope.message.email);
+                            Data.setEmail($scope.message.email);
+                          
+                        } else {
+                            alert($scope.message);
+//                            window.location.reload(true);
+                        }
+                    }).error(function () {
+                            alert("网络错误q");
+//                            window.location.reload(true);
+                        }
+                    );
+                } else {
+                	$scope.errorMsg.general = "验证问题答案错误";
+                    //$scope.createCode();
+                }
+            } else {
+            	$scope.errorMsg.general ="密码为空，请重新输入";
+            }
+        }else{
+        	$scope.errorMsg.general = "所有选项均不可为空";
+        }
+    };
+}]);
---
+++ src/main/webapp/resource/js/controller/tloginCtrl.js
@@ -0,0 +1,65 @@
+OJApp.controller('loginCtrl',['$scope','$http','Data',function ($scope,$http,Data) {
+	
+	//手机的错误信息
+	$scope.errorMsg={};
+	$scope.verifyQtn ={};
+	
+	$scope.mdLogin = function(){
+		 var url = 'https://api.mingdao.com/oauth2/authorize?app_key=EB56F580B240&redirect_uri='+MD_REDIRECT
+		 window.location.href= url;
+	 } 
+	$scope.confirm = function (data) {
+        if ($scope.Lemail && $scope.Lpwd) {
+        	if(data==1){
+        		var pwd = md5($scope.Lpwd);
+        	}else{
+        		var pwd = $scope.Lpwd;
+        	}
+        	
+        	
+            $http({
+                url: WEBROOT+"/user/confirm",
+                method: 'POST',
+                headers: {
+                    "Authorization": Data.token()
+                },
+                data: {"email": $scope.Lemail, "pwd": pwd, "name": $scope.Lname}
+            }).success(function (data) {
+            	console.log("success");
+                $scope.state = data["state"];//1 true or 0 false
+                Data.clear();//清空缓存
+                var name = $scope.Lemail;
+                $scope.message = data["message"];
+                if (data["message"].handler_url != null && data["message"].handler_url !== "") {
+                    name = data["message"].handler_url;
+                }
+                Data.setName(name);
+                Data.setEmail($scope.Lemail);
+                
+
+                if ($scope.state) {
+                    Data.setToken(data["token"]);
+                    Data.setUid($scope.message.uid);
+                    Data.setPrivi($scope.message.privilege);
+                    Data.setTel($scope.message.tel);
+                    Data.setCompany($scope.message.company);
+                    Data.setInvitedleft($scope.message.invited_left);
+                    Data.setLastActive((new Date()).getTime());
+                    $scope.invitedleft = $scope.message.invited_left;
+                   
+                    window.location.href = '#/loginok';
+//                    $scope.name = $scope.message.handler_url;
+                } else {
+                    
+//                    window.location.href = '#/loginok';
+                	$scope.errmsg = data.message.msg;
+                }
+            }).error(function () {
+            	console.log("err");
+//                    alert("网络错误");
+//                    window.location.reload(true);
+                }
+            )
+        }
+    };
+}]);
