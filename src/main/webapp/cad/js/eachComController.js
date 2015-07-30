4f6590823696ce3ad8ac513560d622dac502d5e5
Moon <moon.ghost.jian@gmail.com>
2015-07-30 23:05:30 +0800

    完善挑战赛列表页

--- src/main/webapp/cad/js/eachComController.js
+++ src/main/webapp/cad/js/eachComController.js
@@ -1,7 +1,22 @@
 /**
  * 做题控制器
  */
-OJApp.controller('eachComController',['$scope','$http','CadData',function ($scope,$http,CadData) {
-	
-	
-}])
+OJApp.controller('eachComController', [ '$scope', '$http', 'Data',
+		function($scope, $http, Data) {
+			$scope.renderList = function(status) {
+				$http({
+					url: WEBROOT + "/challenge/getListByStatus",
+					method: 'POST',
+					headers: {
+						"Authorization" : Data.token()
+					},
+					data : {
+						"status" : status
+					}
+				}).success(function(data) {
+					
+				}).error(function() {
+					alert("网络错误");
+				});
+			}
+		} ])
