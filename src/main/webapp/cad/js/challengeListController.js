14f9776b12617c75a2ebf19c02db7ddaffd3fee3
Moon <moon.ghost.jian@gmail.com>
2015-08-01 15:26:03 +0800

    小幅重构

--- src/main/webapp/cad/js/challengeListController.js
+++ src/main/webapp/cad/js/challengeListController.js
@@ -2,7 +2,7 @@
  * 做题控制器
  */
 OJApp.controller('challengeListController',['$scope','$http','CadData',function ($scope,$http,CadData) {
-	$scope.renderList = function(status, challenges) {
+	$scope.renderList = function(status) {
 		$http({
 			url: WEBROOT + '/challenge/getListByStatus',
 			method: 'POST',
@@ -11,15 +11,25 @@ OJApp.controller('challengeListController',['$scope','$http','CadData',function 
 			}
 		}).success(function(data) {
 			if (data.state == 0) {
-				console.log('1111');
-				challenges = data.message;
-				//$scope.unrunChallenges = data.message;
+				//console.log('1111');
+				//challenges = data.message;
+				switch (status) {
+				case 1:
+					$scope.unrunChallenges = data.message;
+					break;
+				case 2:
+					$scope.runningChallenges = data.message;
+					break;
+				default:
+					break;
+				}
+				
 			}
 		}).error(function() {
 			alert("网络错误");
 		});
 	}
 	
-	$scope.renderList(1, $scope.unrunChallenges);
-	$scope.renderList(2, $scope.runningChallenges);
+	$scope.renderList(1);
+	$scope.renderList(2);
 }])
