7979d8416754734bccdb7ea0befb88e1516b0e3a
Moon <moon.ghost.jian@gmail.com>
2015-08-01 16:57:11 +0800

    搭建挑战赛详情页框架

--- src/main/webapp/cad/js/challengeInfoController.js
+++ src/main/webapp/cad/js/challengeInfoController.js
@@ -1,3 +1,8 @@
-/**
- * 
- */
+OJApp.controller('challengeInfoController', ['$scope','$http','Data','$routeParams','$window',function($scope,$http,Data,$routeParams,$window) {
+	$scope.getCompanyInfo = function() {
+		console.log('$scope.getCompanyInfo');
+		console.log($routeParams.signedkey);
+	}
+	
+	$scope.getCompanyInfo();
+}]);
