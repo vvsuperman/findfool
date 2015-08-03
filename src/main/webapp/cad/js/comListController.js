4f293386555679a197faf8187d580a3da16f6375
方成 <13981823565ab>
2015-08-03 14:02:25 +0800

    方成的提交

--- src/main/webapp/cad/js/comListController.js
+++ src/main/webapp/cad/js/comListController.js
@@ -6,23 +6,40 @@ OJApp.controller('comListController',['$scope','$http','CadData',function ($scop
 	$scope.nav = 'cad/page/cadnav.html'
 	$scope.template = 'cad/page/comlist.html';
 	
-	$scope.renderList = function(status, challenges) {
-		$http({
-			url: WEBROOT + '/challenge/getListByStatus',
-			method: 'POST',
-			data: {
-				'status': status
-			}
-		}).success(function(data) {
-			if (data.state == 0) {
-				console.log('1111');
-				//challenges = data.message;
-				$scope.unrunChallenges = data.message;
-			}
-		}).error(function() {
-			alert("网络错误");
-		});
-	}
+//	$scope.renderList = function(status, challenges) {
+//		$http({
+//			url: WEBROOT + '/challenge/getListByStatus',
+//			method: 'POST',
+//			data: {
+//				'status': status
+//			}
+//		}).success(function(data) {
+//			if (data.state == 0) {
+//				console.log('1111');
+//				//challenges = data.message;
+//				$scope.unrunChallenges = data.message;
+//			}
+//		}).error(function() {
+//			alert("网络错误");
+//		});
+//	}
 	
-	$scope.renderList(1, null);
+	$http({
+		url: WEBROOT + '/company/findAll',
+		method: 'POST',
+	}).success(function(data) {
+			console.log(data);
+	}).error(function() {
+		alert("网络错误");
+	});
+	
+//	$scope.renderList(1, null);
+	
+	
+	$scope.myInterval = 5000;
+	$scope.noWrapSlides = false;
+    var slides = $scope.slides = [];
+    
+    slides.push({image:'resource/static/comlistbg.jpg'});
+    slides.push({image:'resource/static/dpbg.jpg'});
 }])
