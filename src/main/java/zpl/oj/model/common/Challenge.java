a59bed22df93a531b2180052fd4600d269bd67ae
hanyaquan <705216797@qq.com>
2015-08-07 17:07:35 +0800

    challenge

---
+++ src/main/java/zpl/oj/model/common/Challenge.java
@@ -0,0 +1,51 @@
+package zpl.oj.model.common;
+
+public class Challenge {
+    private int challengeId;
+	private String challengeName;
+	private String challengeCompany;
+	private String challengeTime;
+	private int challengeScore;
+	private String challengeRank;
+	public int getChallengeId() {
+		return challengeId;
+	}
+	public void setChallengeId(int challengeId) {
+		this.challengeId = challengeId;
+	}
+	public String getChallengeName() {
+		return challengeName;
+	}
+	public void setChallengeName(String challengeName) {
+		this.challengeName = challengeName;
+	}
+	public String getChallengeCompany() {
+		return challengeCompany;
+	}
+	public void setChallengeCompany(String challengeCompany) {
+		this.challengeCompany = challengeCompany;
+	}
+	public String getChallengeTime() {
+		return challengeTime;
+	}
+	public void setChallengeTime(String challengeTime) {
+		this.challengeTime = challengeTime;
+	}
+	public int getChallengeScore() {
+		return challengeScore;
+	}
+	public void setChallengeScore(int i) {
+		this.challengeScore = i;
+	}
+	public String getChallengeRank() {
+		return challengeRank;
+	}
+	public void setChallengeRank(String challengeRank) {
+		this.challengeRank = challengeRank;
+	}
+
+
+    
+	
+	
+}
