7003dc4f48022d18abbce127778a440c88396087
hanyaquan <705216797@qq.com>
2015-09-29 18:27:48 +0800

    修改了bug和页面调整，已经随机试卷的排名问题。

--- src/main/java/zpl/oj/service/imp/ReportService.java
+++ src/main/java/zpl/oj/service/imp/ReportService.java
@@ -162,21 +162,34 @@ public class ReportService {
 	 * */
 	public String getRank(Invite invite){
 		int testid = invite.getTestid();
+		
 		Invite socreInvite = inviteDao.getInvitesByIds(invite.getTestid(), invite.getUid());
-		List<Invite> invites = inviteDao.getInviteByTid(invite.getTestid());
-		List<Integer> scores = new ArrayList<Integer>();
-		for(Invite tmp:invites){
-			scores.add(tmp.getScore());
-		}
-		Collections.sort(scores);
-		int i=scores.size()-1;
-		for(;i>=0;i--){
-			if(scores.get(i)==socreInvite.getScore()){
-				i++;
-				break;
-			}
-		}
-		return i+"/"+scores.size();
+		int parentQuizId=socreInvite.getParentquiz();
+
+		int i= inviteDao.getRankByParent(parentQuizId,testid);
+		i=i+1;
+		 int j= inviteDao.countInviteFinishedByP(parentQuizId);
+//		List<Invite> invites=null;
+//		if(testid!=parentQuizId){
+//		invites=inviteDao.getInviteByParentQuiz(parentQuizId);	
+//			
+//		}else if(testid==parentQuizId){
+//		invites= inviteDao.getInviteByTid(invite.getTestid());
+//		}
+//		List<Integer> scores = new ArrayList<Integer>();
+//		for(Invite tmp:invites){
+//			scores.add(tmp.getScore());
+//		}
+//		Collections.sort(scores);
+//		int i=scores.size()-1;
+//		for(;i>=0;i--){
+//			if(scores.get(i)==socreInvite.getScore()){
+//				i++;
+//				break;
+//			}
+//		}
+		//return i+"/"+scores.size();
+		return i+"/"+j;
 	}
 	
 	
